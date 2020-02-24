96e6eabad4f318792a6531c921bac5a8100b04c5
Arina Okshus <chameleon-lizard@github.com>
2020-02-24 12:27:07 +0300

    Started script

--- install.sh
+++ install.sh
@@ -7,3 +7,9 @@ echo "Pre-installation things :)"
 echo ""
 echo "##################################"
 echo ""
+
+sudo pacman -Syyu
+sudo pacman -S vim
+git clone https://github.com/chameleon-lizard/autorice.git
+cd autorice
+cp -r gebaar i3 wpg xfce4 polybar rofi aurman gtk-2.0 gtk-3.0 gtk-4.0 htop neofetch ranger wal ~/.config
