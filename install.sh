2ad3970c693b67d0815533abe7681d0150b94cf2
Arina Okshus <chameleon-lizard@github.com>
2020-02-29 16:42:58 +0300

    Heavy refactoring

--- install.sh
+++ install.sh
@@ -8,27 +8,38 @@ echo ""
 echo "##################################"
 echo ""
 
+# Updating the system
 sudo pacman -Syyu
-sudo pacman -S vim
-pip install gdown --user
+# Installing all packages
+sudo pacman -S vim thefuck i3 polybar ranger feh jq scrot xclip rofi zathura zathura-cb zathura-ps zathura-djvu zathura-pdf-mupdf neofetch nautilus gnome-terminal
+pip install gdown pywal wpgtk --user
+
+# Downloading configs and putting them into right places
 gdown https://drive.google.com/uc\?id\=1FLgw5H2G6u973GHaNHwq6hjD5Vw6HE_Q  
 tar -xvf backup.tar.xz
-cp -r gebaar i3 wpg xfce4 polybar rofi aurman gtk-2.0 gtk-3.0 gtk-4.0 htop neofetch ranger wal ~/.config
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 cp -r .vim .vimrc .zshrc .oh-my-zsh Scripts ..
-sudo pacman -S thefuck i3 polybar ranger feh jq
-pip install pywal --user
-pip install wpgtk --user
+cp -r gebaar i3 wpg xfce4 polybar rofi aurman gtk-2.0 gtk-3.0 gtk-4.0 htop neofetch ranger wal ~/.config
+
+# Installing WPGTK
 wpg-install.sh
-sudo pacman -S rofi
-sudo pacman -S zathura zathura-cb zathura-ps zathura-djvu zathura-pdf-mupdf
-sudo pacman -S neofetch
+
+# Initializing wallpapers
 mkdir -p ~/Pictures
 mkdir -p ~/Pictures/Wallpapers
 wget https://unsplash.com/photos/vpM6b8lhX_U/download?force=true && mv 'download?force=true' ~/Pictures/Wallpepers/wallpaper.jpg
-echo "Now logout and log into i3wm"
 mv ~/autorice/color.py ~/.local/lib/python3.8/site-packages/wpgtk/data/color.py
-sudo pacman -S nautilus
+
+# Creating config for YCM
 mkdir ~/University
 cp ~/autorice/.ycm_extra_conf.py ~/University/
-sudo pacman -S gnome-terminal
+
+# Installing Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo ""
+echo "##################################"
+echo ""
+echo "Now logout and log into i3wm"
+echo ""
+echo "##################################"
+echo ""
